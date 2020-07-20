87548eb3b25df6af9cd75dc187f85a9e9396da0f
Rudy Hartanto (ICT) <rudy.hartanto@hokben.co.id>
2020-07-20 12:01:22 +0700

    update server bat and vbs

---
+++ runserver.bat
@@ -0,0 +1,18 @@
+
+@echo off
+echo.
+
+cd C:\apiserver
+
+set Path=%NodePackagesPath%\node_modules\.bin;%PATH%
+set Path=%NodePackagesPath%;%PATH%
+
+set NODE_PATH=%NodePackagesPath%\node_modules;%NODE_PATH%
+set NODE_ENV=production
+
+echo Environment variables are successfully added.
+echo. 
+echo. 
+echo. 
+
+node index.js
